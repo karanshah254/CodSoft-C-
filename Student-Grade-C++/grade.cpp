84e6c6b64e833e792bdc12dbef7448762298423d
Karan Shah <karankshah2504@gmail.com>
2023-08-19 18:49:33 +0530

    Task Accomplished

--- Student-Grade-C++/grade.cpp
+++ Student-Grade-C++/grade.cpp
@@ -1,5 +1,7 @@
 /*
 
+By CodeSoft
+
 Create a program that manages student grades. Allow the user
 to input student names and their corresponding grades.
 Calculate the average grade and display it along with the highest
